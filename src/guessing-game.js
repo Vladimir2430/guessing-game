a427da6a180c0be3725c57d56a91b4e8db89655f
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-19 22:56:34 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,30 +2,18 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-    a=min;
-    b=max;
-   	aa=false;
-   	bb=false;
+    
     }
 
     guess() {
-        if(aa){
-    		b=average;
-    	}
-    	else if(bb){
-    	    a=average;
-        }
-    average=Math.ceil(a + ((b - a)/2));
-    return average;
+        
     }
     lower() {
-        aa=true;
-        bb=false
+       
     }
 
     greater() {
-        bb=true;
-    	aa=false;
+       
     }
 }
 
